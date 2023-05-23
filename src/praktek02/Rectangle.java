74110f9b9904cc4fbec928cd826effd8b2ffe038
rizkyjulianto77 <rizkyjulianto405@gmail.com>
2023-05-23 18:10:11 +0800

    Constructor Dengan Parameter

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -7,6 +7,12 @@ public class Rectangle {
         panjang=5;
         lebar=2;
     }
+
+    public Rectangle(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        
+    }
    
    void cetakInfo(){
    System.out.println("========================");
